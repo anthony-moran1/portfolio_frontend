3e2a34107c92a33c3e79dd9009099ce9ac726b8a
Anthony Moran <anthony.moran1@hotmail.com>
2024-02-12 19:00:25 +0000

    Add basic html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <>
+        <h1>{"Portfolio"}</h1>
+        <p>{"Welcome to my portfolio site!"}</p>
+        <p>{"Here is another line!"}</p>
+        </>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
